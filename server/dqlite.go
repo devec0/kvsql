4cc853dac45f17493b2b117a1ca9451393711ba9
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-10-04 15:32:30 +0200

    Simplify server.New

--- server/dqlite.go
+++ server/dqlite.go
@@ -3,16 +3,12 @@ package server
 import (
 	"bufio"
 	"context"
-	"database/sql"
 	"fmt"
 	"net"
 	"net/http"
 	"net/url"
-	"time"
 
-	"github.com/canonical/go-dqlite"
 	"github.com/canonical/go-dqlite/client"
-	"github.com/canonical/go-dqlite/driver"
 	"github.com/freeekanayaka/kvsql/transport"
 	"github.com/pkg/errors"
 )
@@ -65,63 +61,6 @@ func dqliteDial(cert *transport.Cert) client.DialFunc {
 	}
 }
 
-// Register a new Dqlite driver and return the registration name.
-func dqliteDriver(store client.NodeStore, cert *transport.Cert) (string, error) {
-	dial := dqliteDial(cert)
-	timeout := 10 * time.Second
-	driver, err := driver.New(
-		store, driver.WithDialFunc(dial),
-		driver.WithConnectionTimeout(timeout),
-		driver.WithContextTimeout(timeout),
-	)
-	if err != nil {
-		return "", errors.Wrap(err, "create dqlite driver")
-	}
-
-	// Create a unique name to pass to sql.Register.
-	driverIndex++
-	name := fmt.Sprintf("dqlite-%d", driverIndex)
-
-	sql.Register(name, driver)
-
-	return name, nil
-}
-
-// Create a new dqlite node.
-func dqliteNode(id uint64, address string, dir string, cert *transport.Cert) (*dqlite.Node, error) {
-	// Wrap the regular dial function which one that also proxies the TLS
-	// connection, since the raft connect function only supports Unix and
-	// TCP connections.
-	dial := func(ctx context.Context, addr string) (net.Conn, error) {
-		tlsDial := dqliteDial(cert)
-		tlsConn, err := tlsDial(ctx, addr)
-		if err != nil {
-			return nil, err
-		}
-		goUnix, cUnix, err := transport.Socketpair()
-		if err != nil {
-			return nil, errors.Wrap(err, "create pair of Unix sockets")
-		}
-
-		transport.Proxy(goUnix, tlsConn)
-
-		return cUnix, nil
-	}
-
-	node, err := dqlite.New(id, address, dir, dqlite.WithBindAddress("@"), dqlite.WithDialFunc(dial))
-	if err != nil {
-		return nil, errors.Wrap(err, "create dqlite node")
-	}
-
-	if err := node.Start(); err != nil {
-		return nil, errors.Wrap(err, "start dqlite node")
-	}
-
-	return node, nil
-}
-
-var driverIndex = 0
-
 func makeDqliteHandler(addr string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("Upgrade") != "dqlite" {
@@ -157,18 +96,4 @@ func makeDqliteHandler(addr string) http.HandlerFunc {
 
 		transport.Proxy(tlsConn, unixConn)
 	}
-}
-
-func dqliteAdd(ctx context.Context, id uint64, address string, store client.NodeStore, cert *transport.Cert) error {
-	dial := dqliteDial(cert)
-	info := client.NodeInfo{ID: id, Address: address}
-	client, err := client.FindLeader(ctx, store, client.WithDialFunc(dial))
-	if err != nil {
-		return errors.Wrap(err, "find leader")
-	}
-	defer client.Close()
-	if err := client.Add(ctx, info); err != nil {
-		return errors.Wrap(err, "join cluster")
-	}
-	return nil
 }
--- server/server.go
+++ server/server.go
@@ -3,11 +3,14 @@ package server
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"net"
 	"net/http"
 	"time"
 
 	"github.com/canonical/go-dqlite"
 	"github.com/canonical/go-dqlite/client"
+	"github.com/canonical/go-dqlite/driver"
 	"github.com/freeekanayaka/kvsql/server/config"
 	"github.com/freeekanayaka/kvsql/transport"
 	"github.com/pkg/errors"
@@ -29,7 +32,7 @@ func New(dir string) (*Server, error) {
 	}
 
 	// Create the dqlite dial function and driver now, we might need it below to join.
-	name, err := dqliteDriver(cfg.Store, cfg.Cert)
+	name, err := registerDriver(cfg)
 	if err != nil {
 		return nil, err
 	}
@@ -37,45 +40,24 @@ func New(dir string) (*Server, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	// It's safe to open the database object now, since no connection will
+	// be attempted until we actually make use of it.
+	db, err := sql.Open(name, "k8s")
+	if err != nil {
+		return nil, errors.Wrap(err, "open cluster database")
+	}
+
+	// Possibly initialize our ID, address and initial node store content.
 	if cfg.Init != nil {
-		cfg.Address = cfg.Init.Address
-		if len(cfg.Init.Cluster) == 0 {
-			// This is the first node of a new cluster.
-			cfg.ID = 1
-			if err := cfg.Store.Set(context.Background(), []client.NodeInfo{{ID: cfg.ID, Address: cfg.Address}}); err != nil {
-				return nil, errors.Wrap(err, "initialize node store")
-			}
-		} else {
-			servers := make([]client.NodeInfo, len(cfg.Init.Cluster))
-			for i, address := range cfg.Init.Cluster {
-				servers[i].ID = uint64(i + 1) // The ID isn't really used
-				servers[i].Address = address
-			}
-			if err := cfg.Store.Set(context.Background(), servers); err != nil {
-				return nil, errors.Wrap(err, "initialize node store")
-			}
-			// Figure out our ID.
-			db, err := sql.Open(name, "k8s")
-			if err != nil {
-				return nil, errors.Wrap(err, "open cluster database")
-			}
-			id, err := queryMaxServerID(ctx, db)
-			if err != nil {
-				return nil, err
-			}
-			cfg.ID = id + 1
+		if err := initConfig(ctx, cfg, db); err != nil {
+			return nil, err
 		}
 		if err := cfg.Save(dir); err != nil {
 			return nil, err
 		}
 	}
 
-	listener, err := transport.Listen(cfg.Address, cfg.Cert)
-	if err != nil {
-		return nil, err
-	}
-
-	node, err := dqliteNode(cfg.ID, cfg.Address, dir, cfg.Cert)
+	node, err := newNode(cfg, dir)
 	if err != nil {
 		return nil, err
 	}
@@ -84,30 +66,14 @@ func New(dir string) (*Server, error) {
 	mux.HandleFunc("/dqlite", makeDqliteHandler(node.BindAddress()))
 	api := &http.Server{Handler: mux}
 
-	go func() {
-		if err := api.Serve(listener); err != http.ErrServerClosed {
-			panic(err)
-		}
-	}()
-
-	db, err := sql.Open(name, "k8s")
-	if err != nil {
-		return nil, errors.Wrap(err, "open cluster database")
+	if err := startAPI(cfg, api); err != nil {
+		return nil, err
 	}
 
-	// If we are initializing a new node, update the cluster state
+	// If we are initializing a new server, update the cluster state
 	// accordingly.
 	if cfg.Init != nil {
-		if len(cfg.Init.Cluster) == 0 {
-			if err := createServersTable(ctx, db); err != nil {
-				return nil, err
-			}
-		} else {
-			if err := dqliteAdd(ctx, cfg.ID, cfg.Address, cfg.Store, cfg.Cert); err != nil {
-				return nil, err
-			}
-		}
-		if err := insertServer(ctx, db, cfg.ID, cfg.Address); err != nil {
+		if err := initServer(ctx, cfg, db); err != nil {
 			return nil, err
 		}
 	}
@@ -120,6 +86,147 @@ func New(dir string) (*Server, error) {
 	}
 
 	return s, nil
+}
+
+// Register a new Dqlite driver and return the registration name.
+func registerDriver(cfg *config.Config) (string, error) {
+	dial := dqliteDial(cfg.Cert)
+	timeout := 10 * time.Second
+	driver, err := driver.New(
+		cfg.Store, driver.WithDialFunc(dial),
+		driver.WithConnectionTimeout(timeout),
+		driver.WithContextTimeout(timeout),
+	)
+	if err != nil {
+		return "", errors.Wrap(err, "create dqlite driver")
+	}
+
+	// Create a unique name to pass to sql.Register.
+	driverIndex++
+	name := fmt.Sprintf("dqlite-%d", driverIndex)
+
+	sql.Register(name, driver)
+
+	return name, nil
+}
+
+var driverIndex = 0
+
+// Initializes the configuration according to the content of the init.yaml
+// file, possibly obtaining a new node ID.
+func initConfig(ctx context.Context, cfg *config.Config, db *sql.DB) error {
+	servers := []client.NodeInfo{}
+
+	if len(cfg.Init.Cluster) == 0 {
+		servers = append(servers, client.NodeInfo{
+			ID:      1,
+			Address: cfg.Init.Address,
+		})
+	} else {
+		for i, address := range cfg.Init.Cluster {
+			servers = append(servers, client.NodeInfo{
+				ID:      uint64(i + 1), // The ID isn't really used,
+				Address: address,
+			})
+		}
+	}
+
+	if err := cfg.Store.Set(context.Background(), servers); err != nil {
+		return errors.Wrap(err, "initialize node store")
+	}
+
+	if len(cfg.Init.Cluster) == 0 {
+		cfg.ID = 1
+	} else {
+		// Figure out our ID.
+		id, err := queryMaxServerID(ctx, db)
+		if err != nil {
+			return err
+		}
+		cfg.ID = id + 1
+	}
+
+	cfg.Address = cfg.Init.Address
+
+	return nil
+}
+
+// Create a new dqlite node.
+func newNode(cfg *config.Config, dir string) (*dqlite.Node, error) {
+	// Wrap the regular dial function which one that also proxies the TLS
+	// connection, since the raft connect function only supports Unix and
+	// TCP connections.
+	dial := func(ctx context.Context, addr string) (net.Conn, error) {
+		dial := dqliteDial(cfg.Cert)
+		tlsConn, err := dial(ctx, addr)
+		if err != nil {
+			return nil, err
+		}
+		goUnix, cUnix, err := transport.Socketpair()
+		if err != nil {
+			return nil, errors.Wrap(err, "create pair of Unix sockets")
+		}
+
+		transport.Proxy(goUnix, tlsConn)
+
+		return cUnix, nil
+	}
+
+	node, err := dqlite.New(cfg.ID, cfg.Address, dir, dqlite.WithBindAddress("@"), dqlite.WithDialFunc(dial))
+	if err != nil {
+		return nil, errors.Wrap(err, "create dqlite node")
+	}
+
+	if err := node.Start(); err != nil {
+		return nil, errors.Wrap(err, "start dqlite node")
+	}
+
+	return node, nil
+}
+
+// Create and start the server.
+func startAPI(cfg *config.Config, api *http.Server) error {
+	listener, err := transport.Listen(cfg.Address, cfg.Cert)
+	if err != nil {
+		return err
+	}
+	go func() {
+		if err := api.Serve(listener); err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+	return nil
+}
+
+func initServer(ctx context.Context, cfg *config.Config, db *sql.DB) error {
+	if len(cfg.Init.Cluster) == 0 {
+		if err := createServersTable(ctx, db); err != nil {
+			return err
+		}
+	} else {
+		if err := joinCluster(ctx, cfg); err != nil {
+			return err
+		}
+	}
+	if err := insertServer(ctx, db, cfg.ID, cfg.Address); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Make this node join an existing dqlite cluster.
+func joinCluster(ctx context.Context, cfg *config.Config) error {
+	dial := dqliteDial(cfg.Cert)
+	info := client.NodeInfo{ID: cfg.ID, Address: cfg.Address}
+	client, err := client.FindLeader(ctx, cfg.Store, client.WithDialFunc(dial))
+	if err != nil {
+		return errors.Wrap(err, "find leader")
+	}
+	defer client.Close()
+	if err := client.Add(ctx, info); err != nil {
+		return errors.Wrap(err, "join cluster")
+	}
+	return nil
 }
 
 func (s *Server) DB() *sql.DB {
