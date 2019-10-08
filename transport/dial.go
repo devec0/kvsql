6ec117d8bb5531c27c8c89922859c5bcf89b05ba
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-10-08 13:19:28 +0200

    Update TLS configuration

--- transport/dial.go
+++ transport/dial.go
@@ -3,6 +3,8 @@ package transport
 import (
 	"context"
 	"crypto/tls"
+	"crypto/x509"
+	"fmt"
 	"net"
 	"time"
 
@@ -18,9 +20,21 @@ func Dial(ctx context.Context, cert *Cert, addr string) (net.Conn, error) {
 	dialer := &net.Dialer{Timeout: timeout}
 
 	cfg := &tls.Config{
-		RootCAs:      cert.Pool,
-		Certificates: []tls.Certificate{cert.KeyPair},
+		MinVersion:               tls.VersionTLS12,
+		CipherSuites:             tlsCipherSuites,
+		PreferServerCipherSuites: true,
+		RootCAs:                  cert.Pool,
+		Certificates:             []tls.Certificate{cert.KeyPair},
 	}
+
+	crt, err := x509.ParseCertificate(cert.KeyPair.Certificate[0])
+	if err != nil {
+		return nil, errors.Wrap(err, "parse keypair certificate")
+	}
+	if len(crt.DNSNames) == 0 {
+		return nil, fmt.Errorf("certificate has no DNS extension")
+	}
+	cfg.ServerName = crt.DNSNames[0]
 
 	conn, err := tls.DialWithDialer(dialer, "tcp", addr, cfg)
 	if err != nil {
--- transport/listen.go
+++ transport/listen.go
@@ -13,17 +13,13 @@ func Listen(addr string, cert *Cert) (net.Listener, error) {
 	// See https://github.com/denji/golang-tls
 	cfg := &tls.Config{
 		MinVersion:               tls.VersionTLS12,
-		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		CipherSuites:             tlsCipherSuites,
 		PreferServerCipherSuites: true,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-		},
-		Certificates: []tls.Certificate{cert.KeyPair},
-		ClientCAs:    cert.Pool,
-		ClientAuth:   tls.RequireAndVerifyClientCert,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		Certificates:             []tls.Certificate{cert.KeyPair},
+		RootCAs:                  cert.Pool,
+		ClientCAs:                cert.Pool,
+		ClientAuth:               tls.RequireAndVerifyClientCert,
 	}
 	cfg.BuildNameToCertificate()
 
