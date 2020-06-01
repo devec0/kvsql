d1d36a44af71906f58736e44b70dc2d4914feead
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-06-01 11:38:05 +0100

    Rest API

--- rest.go
+++ rest.go
@@ -7,4 +7,20 @@ import (
 type Rest struct{}
 
 func (r Rest) Install(c *restful.Container) {
+	ws := new(restful.WebService)
+	ws.Path("/dqlite").Consumes(restful.MIME_JSON).Produces(restful.MIME_JSON)
+	ws.Doc("dqlite cluster management")
+	ws.Route(ws.GET("/").To(getHandler))
+	c.Add(ws)
+}
+
+func getHandler(req *restful.Request, resp *restful.Response) {
+	foo := struct {
+		A string
+		B int
+	}{
+		A: "foo",
+		B: 123,
+	}
+	resp.WriteEntity(foo)
 }
