bb2cbd934ec31e52452dccbde6ef767c45533429
James Hebden <james@ec0.io>
2020-11-06 17:47:18 +1100

    Start adding logging, use logrus as that's what kine uses, and so we can set trace level logging for now

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/canonical/go-dqlite v1.8.0
+	github.com/devec0/kine v0.5.2
 	github.com/emicklei/go-restful v2.13.0+incompatible
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
@@ -11,8 +12,6 @@ require (
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/pkg/errors v0.9.1
-	github.com/rancher/kine v0.5.0
-	github.com/sirupsen/logrus v1.6.0 // indirect
 	github.com/stretchr/testify v1.6.0
 	go.etcd.io/etcd v0.0.0-20191023171146-3cf2f69b5738
 	go.uber.org/zap v1.15.0 // indirect
@@ -22,4 +21,7 @@ require (
 	gopkg.in/yaml.v2 v2.3.0
 	k8s.io/apimachinery v0.17.0
 	k8s.io/apiserver v0.17.0
+	k8s.io/klog/v2 v2.4.0
 )
+
+replace github.com/rancher/kine => github.com/devec0/kine v0.5.1
--- server/server.go
+++ server/server.go
@@ -7,14 +7,15 @@ import (
 	"os"
 	"path/filepath"
 	"time"
-
+	
 	"github.com/canonical/go-dqlite"
 	"github.com/canonical/go-dqlite/app"
 	"github.com/canonical/go-dqlite/client"
 	"github.com/devec0/kvsql/server/config"
 	"github.com/ghodss/yaml"
 	"github.com/pkg/errors"
-	"github.com/rancher/kine/pkg/endpoint"
+	"github.com/devec0/kine/pkg/endpoint"
+	log "k8s.io/klog/v2"
 )
 
 // Server sets up a single dqlite node and serves the cluster management API.
@@ -26,12 +27,15 @@ type Server struct {
 }
 
 func New(dir string) (*Server, error) {
+
+	log.Info("Creating new kvsql instance")
 	// Check if we're initializing a new node (i.e. there's an init.yaml).
 	cfg, err := config.Load(dir)
 	if err != nil {
 		return nil, err
 	}
 
+	log.Info("Loaded configuration for kvsql/kine/dqline node")
 	if cfg.Update != nil {
 		info := client.NodeInfo{}
 		path := filepath.Join(dir, "info.yaml")
@@ -42,6 +46,7 @@ func New(dir string) (*Server, error) {
 		if err := yaml.Unmarshal(data, &info); err != nil {
 			return nil, err
 		}
+		log.Infof("kvsql setting dqlite addresses: %v", info)
 		info.Address = cfg.Update.Address
 		data, err = yaml.Marshal(info)
 		if err != nil {
@@ -50,14 +55,17 @@ func New(dir string) (*Server, error) {
 		if err := ioutil.WriteFile(path, data, 0600); err != nil {
 			return nil, err
 		}
+		log.Info("kvsql configuring dqlite membership")
 		nodes := []dqlite.NodeInfo{info}
 		if err := dqlite.ReconfigureMembership(dir, nodes); err != nil {
 			return nil, err
 		}
+		log.Info("kvsql configuring dqlite YAML store")
 		store, err := client.NewYamlNodeStore(filepath.Join(dir, "cluster.yaml"))
 		if err != nil {
 			return nil, err
 		}
+		log.Info("kvsql setting dqlite datastore")
 		if err := store.Set(context.Background(), nodes); err != nil {
 			return nil, err
 		}
@@ -73,33 +81,44 @@ func New(dir string) (*Server, error) {
 
 	// Possibly initialize our ID, address and initial node store content.
 	if cfg.Init != nil {
+		log.Info("Attempting to initialise the kvsql/kine/dqline cluster")
 		options = append(options, app.WithAddress(cfg.Init.Address), app.WithCluster(cfg.Init.Cluster))
 	}
 
+	log.Info("kvsql booting dqlite app")
 	app, err := app.New(dir, options...)
 	if err != nil {
 		return nil, err
 	}
 	if cfg.Init != nil {
+		log.Info("Removing kvsql cluster init.yaml now that we are initialised")
 		if err := os.Remove(filepath.Join(dir, "init.yaml")); err != nil {
 			return nil, err
 		}
 	}
 
+	log.Info("kvsql creating context and deferring")
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
 	if err := app.Ready(ctx); err != nil {
+		log.Info("kvsql error on ready context: %v", err)
 		return nil, err
 	}
 
+	log.Info("kvsql configuring dqlite/kine endpoints")
 	peers := filepath.Join(dir, "cluster.yaml")
 	config := endpoint.Config{
-	        Listener: "localhost:12379",
+	        Listener: "tcp://127.0.0.1:12379",
 		Endpoint: fmt.Sprintf("dqlite://k8s?peer-file=%s&driver-name=%s", peers, app.Driver()),
 	}
+	
 	kineCtx, cancelKine := context.WithCancel(context.Background())
+	log.Infof("Starting kine listener on %s", config.Listener)
+	
 	if _, err := endpoint.Listen(kineCtx, config); err != nil {
+		log.Infof("Kine context exited in error: %v", err)
+		<-kineCtx.Done()
 		return nil, errors.Wrap(err, "kine")
 	}
 
@@ -110,15 +129,18 @@ func New(dir string) (*Server, error) {
 		cancelKine: cancelKine,
 	}
 
+	log.Infof("kvsql returning new server instance: %s", cfg.Address)
 	return s, nil
 }
 
 func (s *Server) Close(ctx context.Context) error {
 	if s.cancelKine != nil {
+		log.Info("Cancelling kine context")
 		s.cancelKine()
 	}
 	s.app.Handover(ctx)
 	if err := s.app.Close(); err != nil {
+		log.Info("Stopped dqlite and requested handover")
 		return errors.Wrap(err, "stop dqlite app")
 	}
 	return nil
